90972d91fe17f0008248f92d12ca0cf524a1e31d
TylerLN <tylerlngu@csu.fullerton.edu>
2024-09-22 17:59:33 -0700

    Format and Cleaned a bit

--- fib.c
+++ fib.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#define MEMO_ARRAY_SIZE 10000
 
-unsigned long long int memo_array[INT_MAX];
+unsigned long long int memo_array[MEMO_ARRAY_SIZE];
 
 unsigned long long int fib_i_core(int n)
 {
@@ -14,7 +14,11 @@ unsigned long long int fib_i_core(int n)
    {
       return 1;
    }
-   unsigned long long int first = 0, second = 1, next_value;
+
+   unsigned long long int first = 0;
+   unsigned long long int second = 1;
+   unsigned long long int next_value;
+
    for (int i = 3; i <= n; i++)
    {
       next_value = first + second;
@@ -46,30 +50,15 @@ unsigned long long int FibIterative(int n)
    {
       return memo_array[n];
    }
-   if (n <= 1)
-   {
-      return 0;
-   }
-   else if (n == 2)
-   {
-      return 1;
-   }
    memo_array[n] = fib_i_core(n);
    return memo_array[n];
 }
+
 unsigned long long int FibRecursive(int n)
 {
    if (memo_array[n] != 0)
    {
       return memo_array[n];
-   }
-   if (n <= 1)
-   {
-      return 0;
-   }
-   else if (n == 2)
-   {
-      return 1;
    }
    memo_array[n] = fib_r_core(n);
    return memo_array[n];
@@ -77,18 +66,16 @@ unsigned long long int FibRecursive(int n)
 
 int main(int argc, char *argv[])
 {
-   int value = atoi(argv[1]);
-
-   int N = value;
+   int user_int = atoi(argv[1]);
 
    if (argv[2][0] == 'i')
    {
-      unsigned long long int result = FibIterative(N);
+      unsigned long long int result = FibIterative(user_int);
       printf("%llu", result);
    }
    else if (argv[2][0] == 'r')
    {
-      unsigned long long int result = FibRecursive(N);
+      unsigned long long int result = FibRecursive(user_int);
       printf("%llu", result);
    }
    return 0;
