dc8bd5148d6f985d42168eca1989564f11f8bd9f
TylerLN <tylerlngu@csu.fullerton.edu>
2024-09-23 10:49:15 -0700

    Google Format

--- fib.c
+++ fib.c
@@ -4,79 +4,70 @@
 
 unsigned long long int memo_array[MEMO_ARRAY_SIZE];
 
-unsigned long long int fib_i_core(int n)
-{
-   if (n == 1)
-   {
-      return 0;
-   }
-   else if (n == 2)
-   {
-      return 1;
-   }
+unsigned long long int fib_i_core(int n) {
+  if (n == 1) {
+    return 0;
+  } else if (n == 2) {
+    return 1;
+  }
 
-   unsigned long long int first = 0;
-   unsigned long long int second = 1;
-   unsigned long long int next_value;
+  unsigned long long int first = 0;
+  unsigned long long int second = 1;
+  unsigned long long int next_value;
 
-   for (int i = 3; i <= n; i++)
-   {
-      next_value = first + second;
-      first = second;
-      second = next_value;
-   }
-   return second;
+  for (int i = 3; i <= n; i++) {
+    next_value = first + second;
+    first = second;
+    second = next_value;
+  }
+  return second;
 }
 
-unsigned long long int fib_r_core(int n)
-{
-   if (n == 1)
-   {
-      return 0;
-   }
-   else if (n == 2)
-   {
-      return 1;
-   }
-   else
-   {
-      return fib_r_core(n - 1) + fib_r_core(n - 2);
-   }
+unsigned long long int fib_r_core(int n) {
+  if (n == 1) {
+    return 0;
+  } else if (n == 2) {
+    return 1;
+  } else {
+    return fib_r_core(n - 1) + fib_r_core(n - 2);
+  }
 }
 
-unsigned long long int FibIterative(int n)
-{
-   if (memo_array[n] != 0)
-   {
-      return memo_array[n];
-   }
-   memo_array[n] = fib_i_core(n);
-   return memo_array[n];
+unsigned long long int FibIterative(int n) {
+  if (memo_array[n] != 0) {
+    return memo_array[n];
+  }
+  if (n <= 1) {
+    return 0;
+  } else if (n == 2) {
+    return 1;
+  }
+  memo_array[n] = fib_i_core(n);
+  return memo_array[n];
 }
 
-unsigned long long int FibRecursive(int n)
-{
-   if (memo_array[n] != 0)
-   {
-      return memo_array[n];
-   }
-   memo_array[n] = fib_r_core(n);
-   return memo_array[n];
+unsigned long long int FibRecursive(int n) {
+  if (memo_array[n] != 0) {
+    return memo_array[n];
+  }
+  if (n <= 1) {
+    return 0;
+  } else if (n == 2) {
+    return 1;
+  }
+  memo_array[n] = fib_r_core(n);
+  return memo_array[n];
 }
 
-int main(int argc, char *argv[])
-{
-   int user_int = atoi(argv[1]);
+int main(int argc, char *argv[]) {
+  int user_int = atoi(argv[1]);
 
-   if (argv[2][0] == 'i')
-   {
-      unsigned long long int result = FibIterative(user_int);
-      printf("%llu", result);
-   }
-   else if (argv[2][0] == 'r')
-   {
-      unsigned long long int result = FibRecursive(user_int);
-      printf("%llu", result);
-   }
-   return 0;
+  if (argv[2][0] == 'i') {
+    unsigned long long int result = FibIterative(user_int);
+    printf("%llu", result);
+  } else if (argv[2][0] == 'r') {
+    unsigned long long int result = FibRecursive(user_int);
+    printf("%llu", result);
+  }
+  return 0;
 }
